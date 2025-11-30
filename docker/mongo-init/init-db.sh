ff75fb30c7862172e6292c09048eb6c95d11f8c7
Gianpio Silvestri <Sceptile72@gmail.com>
2025-11-30 19:59:50 +0100

    fix: fixed mongo import path

--- docker/mongo-init/init-db.sh
+++ docker/mongo-init/init-db.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 echo "🐳 STARTING DATA IMPORT..."
 
-mongoimport --host localhost --db Chemo --collection users --drop --file init/users.json --jsonArray
-mongoimport --host localhost --db Chemo --collection medicine --drop --file init/medicine.json --jsonArray
-mongoimport --host localhost --db Chemo --collection patient --drop --file init/patient.json --jsonArray
-mongoimport --host localhost --db Chemo --collection planner --drop --file init/planner.json --jsonArray
+mongoimport --host localhost --db Chemo --collection user --drop --file /data/seed/init/user.json --jsonArray
+mongoimport --host localhost --db Chemo --collection medicine --drop --file /data/seed/init/medicine.json --jsonArray
+mongoimport --host localhost --db Chemo --collection patient --drop --file /data/seed/init/patient.json --jsonArray
+mongoimport --host localhost --db Chemo --collection planner --drop --file /data/seed/init/planner.json --jsonArray
 
 echo "✅ IMPORT COMPLETED."
